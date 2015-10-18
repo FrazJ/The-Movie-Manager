9bcd4d7ffdd7cef9646c8c74c4146b296e65da63
Frazer Hogg <frazerj@me.com>
2015-10-18 19:17:39 +0100

    POST method implemented

--- TheMovieManager/TheMovieManager/TMDBClient.swift
+++ TheMovieManager/TheMovieManager/TMDBClient.swift
@@ -144,19 +144,48 @@ class TMDBClient : NSObject {
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         
-        request.HTTPBody = "{\"\(JSONBodyKeys.MediaType)\": \"\(jsonBody[JSONBodyKeys.MediaType])\",\"\(JSONBodyKeys.MediaID)\": \"\(jsonBody[JSONBodyKeys.MediaID])\",\"\(JSONBodyKeys.Favorite)\": \"\(jsonBody[JSONBodyKeys.Favorite])}".dataUsingEncoding(NSUTF8StringEncoding)
-
+        do {
+            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(jsonBody, options: .PrettyPrinted)
+        } catch {
+            print("The JSON could not be serialised")
+        }
+        
     
         /* 4. Make the request */
-        
-        
-        
-        /* 5/6. Parse the data and use the data (happens in completion handler) */
-        
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                print("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                if let response = response as? NSHTTPURLResponse {
+                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+                } else if let response = response {
+                    print("Your request returned an invalid response! Response: \(response)!")
+                } else {
+                    print("Your request returned an invalid response!")
+                }
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            
+            /* 5/6. Parse the data and use the data (happens in completion handler) */
+            TMDBClient.parseJSONWithCompletionHandler(data, completionHandler: completionHandler)
+        }
         
         /* 7. Start the request */
+        task.resume()
         
-        return NSURLSessionDataTask()
+        return task
     }
     
     /* Use this unFavoriteButtonTouchUpInside as a reference if you need it 😄 */
